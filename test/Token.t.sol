2fae4afc26a7418fa33333e638c9f7a66a8e3c6c
lsqrl <lsqrl91@gmail.com>
2024-06-07 10:59:35 +0200

    rename

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Token} from "../src/Token.sol";
 
 contract CounterTest is Test {
     Counter public counter;
