b3f439e5078827c9ca66a330c269fbc187d6981b
lsqrl <lsqrl91@gmail.com>
2024-06-07 11:49:20 +0200

    verify script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 rpc_url=$(grep RPC_URL .env | cut -d '=' -f2)
 private_key=$(grep PRIVATE_KEY .env | cut -d '=' -f2)
+arbiscan_api_key=$(grep ARBISCAN_API_KEY .env | cut -d '=' -f2)
 
-forge create --rpc-url $rpc_url --constructor-args "Test token" "TSTTKN" 18 1000000 --private-key $private_key src/Token.sol:Token
+forge create --rpc-url $rpc_url --constructor-args "Stablecoin" "STB" 6 1000000 --etherscan-api-key $arbiscan_api_key --verify --private-key $private_key src/Token.sol:Token
